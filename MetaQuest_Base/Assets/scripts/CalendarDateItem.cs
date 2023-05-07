2e68495ee09522dd8765dda4a4dda82954e25478
sayyes304 <yhs7409@naver.com>
2023-05-07 18:38:44 +0900

    Add Calender Date

---
+++ MetaQuest_Base/Assets/scripts/CalendarDateItem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CalendarDateItem : MonoBehaviour {
+    //public Text DateText;
+
+    public GameObject DailyText;
+    public GameObject DailyLine;
+
+    public Transform DailyPlan_Parent;
+    
+    public void OnDateItemClick()
+    {
+        // CalendarController._calendarInstance.OnDateItemClick(DateText.text);
+        // DateText.gameObject.SetActive(true);
+        // gameObject.GetComponent<Button>(). = ;
+        CalendarController._calendarInstance.OnDateItemClick(gameObject.GetComponentInChildren<Text>().text);
+
+        print("name : " + gameObject.GetComponent<RectTransform>().anchoredPosition + " name ? : " + gameObject.name + " day : " + CalendarController._calendarInstance.Daily_day);
+
+        // 일정 text 생성
+        GameObject DailyPlan = Instantiate(DailyText, DailyPlan_Parent);
+        DailyPlan.transform.name = CalendarController._calendarInstance.Daily_day;
+
+        
+        GameObject Daily = Instantiate(DailyLine, DailyPlan.transform);
+
+        Daily.transform.position = new Vector3(transform.position.x, transform.position.y - 10, transform.position.z);
+
+    }
+}
